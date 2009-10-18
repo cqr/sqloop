89c53c9a204111383ff47ad5bb6419fbf76620e8
chrisrhoden <carhoden@gmail.com>
2009-10-18 15:39:06 -0400

    some scaffolding

---
+++ sqloop.php
@@ -0,0 +1,25 @@
+<?php
+class SqloopBase extends betterBase
+{
+    function __construct() {
+        
+    }
+    
+    function __call($propname, $args) {
+        
+    }
+    
+    static function find() {
+        return $this;
+    }
+    
+    static
+    
+}
+
+class SqloopQuery extends betterBase
+{
+    function __construct($table) {
+        
+    }
+}
